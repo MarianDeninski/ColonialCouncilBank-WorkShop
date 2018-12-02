59a0844ac3081a1f3fffa942f47cf3d0b1ec8c89
Marian <marian.deninski@gmail.com>
2018-12-02 18:09:09 +0200

    first commit

--- src/main/java/app/ccb/util/FileUtil.java
+++
@@ -1,9 +0,0 @@
-package app.ccb.util;
-
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
-public interface FileUtil {
-
-    String readFile(String filePath) throws IOException;
-}
--- src/main/java/app/ccb/util/ValidationUtil.java
+++
@@ -1,6 +0,0 @@
-package app.ccb.util;
-
-public interface ValidationUtil {
-
-    <E> boolean isValid(E entity);
-}
